c67b38b36d1ece6b8d6c53bf105181206e719b8c
Nurat Lawal <nurahawwal@yahoo.com>
2024-04-11 00:27:36 +0100

    updated addproduct

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const errorHandler= require('./backend/middleware/errorHandler')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use(cors());
+// app.use(cors());
 app.use((req, res, next) => {
     res.setHeader(
       "Access-Control-Allow-Origin",
-      "*"
+      "http://127.0.0.1:5500"
     );
     res.setHeader(
       "Access-Control-Allow-Methods",
