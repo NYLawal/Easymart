b0b3d3b157010d89b0980752222512de8e731d4b
Nurat Lawal <nurahawwal@yahoo.com>
2024-04-11 00:05:12 +0100

    updated addproduct

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.setHeader(
       "Access-Control-Allow-Origin",
-      "https://your-frontend.com"
+      "*"
     );
     res.setHeader(
       "Access-Control-Allow-Methods",
@@ -55,7 +55,8 @@ app.options("*", (req, res) => {
       console.log("pass");
       return res.status(204).send();
     } else {
-      console.log("fail");
+    }
+    })
 // app.use(express.static('public'))
 
 
